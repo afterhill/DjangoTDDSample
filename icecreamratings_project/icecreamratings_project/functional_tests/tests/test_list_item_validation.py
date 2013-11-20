1542e9a75c0864e544d7ad7718ab6658259e37ef
mengjuesh <mengjue@outlook.com>
2013-11-20 23:39:10 +0800

    simple javascript validation passed

--- icecreamratings_project/icecreamratings_project/functional_tests/tests/test_list_item_validation.py
+++ icecreamratings_project/icecreamratings_project/functional_tests/tests/test_list_item_validation.py
@@ -12,7 +12,7 @@ class ItemValidationTest(FunctionalTest):
 
         inputbox.send_keys('\n')
 
-        error = self.browser.find_element_by_css_selector('.has-error')  # 1
+        error = self.get_error_element()
         self.assertEqual(error.text, EMPTY_LIST_ERROR)
 
         inputbox = self.get_item_input_box()
@@ -22,7 +22,7 @@ class ItemValidationTest(FunctionalTest):
         inputbox = self.get_item_input_box()
         inputbox.send_keys('\n')
 
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.assertEqual(error.text, EMPTY_LIST_ERROR)
 
         inputbox = self.get_item_input_box()
@@ -39,5 +39,20 @@ class ItemValidationTest(FunctionalTest):
 
         self.check_for_row_in_list_table('1: Buy wellies')
 
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.assertEqual(error.text, DUPLICATE_ITEM_ERROR)
+
+    def test_error_message_are_cleared_on_input(self):
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys('\n')
+        error = self.get_error_element()
+
+
+        self.get_item_input_box().send_keys('a')
+
+        error = self.get_error_element()
+
+        self.assertFalse(error.is_displayed())
+
+    def get_error_element(self):
+        return self.browser.find_element_by_css_selector('.has-error')
